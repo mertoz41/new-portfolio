92288a400ebfe36969e119ecc3a08a769a3b7f32
bob <mertozka41@gmail.com>
2021-12-04 18:27:13 -0500

    rxperiment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Pic from './images/mert-cropped.jpg'
 import { Button } from 'semantic-ui-react'
-
+import CezsBir from './images/portfolio2.jpg'
+import CezsIki from './images/portfolioshot.jpg'
 function App() {
   const toWeb = (web) =>{
     window.open(web, '_blank')
@@ -12,33 +13,64 @@ function App() {
         <div className="top">
           <img src={Pic} className="picture"/>
           <div className="info">
-              <h1>Mert Özkaynak</h1>
             <div className="about">
+              <div>
+                <h1>Mert Özkaynak</h1>
+                
+              </div>
+              <div>
                   <Button circular icon="github" onClick={() => toWeb('https://github.com/mertoz41')} />
                   <Button circular color="linkedin" icon="linkedin" onClick={() => this.toWeb('https://www.linkedin.com/in/mert-ozkaynak/')}/>
-            
-    
+
             </div>
-                {/* <div className="button"> */}
-                {/* </div> */}
-              <h4>Alexandria, VA</h4>
-              <h3>Full-Stack App Developer</h3>
+              
+
+            </div>
+            <div className="place">
+                  <div>
+                  <h3>Full-Stack App Developer</h3>
+
+                  </div>
+                  <div>
+                  <h3>Alexandria, VA</h3>
+
+                  </div>
+
+                </div>
+              <div className="skills">
+          <h2>technical skills</h2>
+            <p>JavaScript, React, Redux,  Node.js, Express, MongoDB, React Native, React Navigation, Ruby, Ruby on Rails, SQL, PostgreSQL, HTML, CSS</p>
+        </div>
           </div>
 
         </div>
-        <div className="skills">
-          <h2>technical skills</h2>
-            <p>Javascript, React, Redux,  Node.js, Express, MongoDB, React Native, React Navigation, Ruby, Ruby on Rails, SQL, PostgreSQL, HTML, CSS</p>
-        </div>
-        <div className="projects">
+        
+        <div className="section">
           <h2>projects</h2>
-          <h3>Instrumentalists app</h3>
-          <h3>Artstone Marble and Granite Offical Website</h3>
-          <h3>Project Tracker app</h3>
-          <h3>Beatbox Bubble app</h3>
-          <h3>Hooper app</h3>
+          <div className="projek">
+            <h3>Instrumentalists app</h3>
+            <p>Social network for instrumentalists</p>
+            <img src={CezsBir} className="picture"/>
+            <img src={CezsIki} className="picture"/>
+          </div>
+          <div className="projek">
+            <h3>Artstone Marble and Granite Offical Website</h3>
+
+          </div>
+          <div className="projek">
+            <h3>Project Tracker app</h3>
+
+          </div>
+          <div className="projek">
+            <h3>Beatbox Bubble app</h3>
+
+          </div>
+          <div className="projek">
+            <h3>Hooper app</h3>
+
+          </div>
         </div>
-        <div className="story">
+        <div className="section">
           <h2>story</h2>
           <p>Born and raised in Izmir, Turkey, I came to the US in 2010, seeking to continue my education as a student-athlete. 
             Upon graduating from T.C. Williams High School in 2014 and Thiel College in 2018 with a bachelors degree in International Business studies, I played professional basketball for a year with Sigortam Net ITU Basket in Istanbul. 
@@ -46,7 +78,7 @@ function App() {
             So far it has been a smooth transition as I channel the discipline and work ethic gained from basketball into this nonstop evolving industry. 
             I spend most of my time working on projects, studying computer science fundamentals, and incorporating new technologies in quest to mastery.</p>
         </div>
-        <div className="contact">
+        <div className="section">
           <h2>contact me hoe</h2>
 
         </div>
