1fdbe1231dc331e7afaf8203660c41abd4fe3e54
bob <mertozka41@gmail.com>
2021-12-11 01:09:51 -0500

    landing project added

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ import { Button, Modal } from 'semantic-ui-react'
 import emailjs from 'emailjs-com'
 import { Helmet } from 'react-helmet'
 import Clouds from './images/clouds.mp4'
+import Landinbir from './images/landinbir.jpg'
+import Landiniki from './images/landiniki.jpg'
+import Landinuc from './images/landinuc.jpg'
 
 function App() {
   const [open, setOpen] = useState(false)
@@ -79,6 +82,13 @@ const sendEmail = e => {
     <Button circular icon="github" onClick={() => toWeb('https://github.com/mertoz41')} />        
 
     </div>
+    <div className='bottomSection'>
+
+    <div className="button" onClick={() => toWeb('https://github.com/mertoz41/new-portfolio')}>
+                  <h4>portfolio code</h4>
+        </div>
+        </div>
+
     <div>
     
 
@@ -90,13 +100,11 @@ const sendEmail = e => {
 <div className='section'>
         
         
-            <p>JavaScript, React, React Native, Gatsby, Redux, Ruby, Ruby on Rails, SQL, PostgreSQL, HTML, CSS</p>
+            <p>JavaScript, React, React Native, Gatsby, Redux, Ruby, Ruby on Rails, SQL, PostgreSQL, Sqlite3, HTML, CSS</p>
 
 </div>
       
-        {/* <div className="button" onClick={() => toWeb('https://github.com/mertoz41/new-portfolio')}>
-                  <h4>portfolio code</h4>
-        </div> */}
+        
         
           <h2 className="userTitle">projects</h2>
         <div>
@@ -112,9 +120,7 @@ const sendEmail = e => {
                 <div className="button">
                 <h4 onClick={() => toWeb('https://youtu.be/X6erehzcO3g')}>demo</h4>
               </div>
-              <div className="button">
-                <h4 onClick={() => toWeb('http://cezsmusic.com')}>website</h4>
-              </div>
+  
               <div className="button">
                 <h4 onClick={() => toWeb('https://github.com/mertoz41/native-front')}>code</h4>
               </div>
@@ -130,11 +136,10 @@ const sendEmail = e => {
               <img onClick={() => selectPicture(CezsUc, "native")}src={CezsUc} className="phonepic" alt='cez3'/>
 
             </div>
-            <p>PostgreSQL, Ruby on Rails, React Native, Expo</p>
-            <p>Spotify, Here Geolocation, JWT </p>
-
-
-
+            <div className='stackSection'>
+              <p><span className="coloredFont">backend</span> Rails, PostgreSQL, Redis, JWT, streamio-ffmpeg, ActionCable, ActiveJob, ActiveStorage</p>
+              <p><span className="coloredFont">frontend</span> React Native, Expo, AsyncStorage, React Navigation, React Native Maps, Here API, Spotify API</p>
+            </div>
           </div>
           <div className="projek">
             <div className="titleSection">
@@ -144,14 +149,11 @@ const sendEmail = e => {
               </div>
 
               <div className="buttons">
-                <div className="button">
-                <h4 onClick={() => toWeb('https://youtu.be/X6erehzcO3g')}>demo</h4>
-              </div>
               <div className="button">
                 <h4 onClick={() => toWeb('http://cezsmusic.com')}>website</h4>
               </div>
               <div className="button">
-                <h4 onClick={() => toWeb('https://github.com/mertoz41/native-front')}>code</h4>
+                <h4 onClick={() => toWeb('https://github.com/mertoz41/landing')}>code</h4>
               </div>
             </div>
             </div>
@@ -160,12 +162,12 @@ const sendEmail = e => {
               Social network designed for instrumentalists where users make up the library with instrument videos, pinpoint upcoming events, and interact with others through music.
             </p>
             <div className="images">
-              {/* <img onClick={() => selectPicture(CezsBir, "native")} src={CezsBir} className="phonepic" alt='cezs1'/>
-              <img onClick={() => selectPicture(CezsIki, "native")}src={CezsIki} className="phonepic" alt='cez2'/>
-              <img onClick={() => selectPicture(CezsUc, "native")}src={CezsUc} className="phonepic" alt='cez3'/> */}
+              <img onClick={() => selectPicture(Landinbir, "native")} src={Landinbir} className="phonepic" alt='cezs1'/>
+              <img onClick={() => selectPicture(Landiniki, "native")}src={Landiniki} className="phonepic" alt='cez2'/>
+              <img onClick={() => selectPicture(Landinuc, "native")}src={Landinuc} className="phonepic" alt='cez3'/>
 
             </div>
-
+            <p><span className="coloredFont">frontend</span> React, EmailJs, React Router, React Page Scroller</p>
           </div>
           <div className="projek">
           <div className="titleSection">
@@ -185,6 +187,9 @@ const sendEmail = e => {
               <img onClick={() => selectPicture(Arttwo, "web")} src={Arttwo} className="projectpic" alt='art2'/>
               <img onClick={() => selectPicture(Arttre, "web")} src={Arttre} className="projectpic" alt='art3'/>
 
+            </div>
+            <div className='stackSection'>
+              <p><span className="coloredFont">frontend</span> Gatsby, bootstrap</p>
             </div>
           </div>
           <div className="projek">
@@ -212,7 +217,11 @@ const sendEmail = e => {
               <img onClick={() => selectPicture(Trackeruno, "web")}src={Trackeruno} className="projectpic" alt='trak3'/>
 
             </div>
+            <div className='stackSection'>
 
+            <p><span className="coloredFont">backend</span> Rails, PostgreSQL, JWT, Bcrypt</p>
+            <p><span className="coloredFont">frontend</span> React, Semantic UI, Redux, React Router</p>
+              </div>
 
           </div>
           <div className="projek">
@@ -244,7 +253,11 @@ const sendEmail = e => {
               <img onClick={() => selectPicture(Bubledos, "web")} src={Bubledos} className="projectpic" alt='bub3'/>
 
             </div>
+            <div className='stackSection'>
 
+            <p><span className="coloredFont">backend</span> Rails, sqlite3, JWT, ActiveStorage</p>
+            <p><span className="coloredFont">frontend</span> React, Semantic UI, Redux, React Router, React Mic, Wavesurfer.js</p>
+              </div>
           </div>
           <div className="projek">
           <div className="titleSection">
@@ -275,7 +288,11 @@ const sendEmail = e => {
 
 
             </div>
+            <div className='stackSection'>
 
+            <p><span className="coloredFont">backend</span> Rails, PostgreSQL, JWT</p>
+            <p><span className="coloredFont">frontend</span> React, Semantic UI, Redux, React Router, React Google Maps</p>
+              </div>
 
           </div>
           <Modal
