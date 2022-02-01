f9607aa48a43ee4b97ca41cf5d189d31935e777d
mert <mertozka41@gmail.com>
2022-02-01 12:07:33 -0500

    good progress

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,8 @@ const About = () => {
         <div className="section">
             <img src={Mertito} alt="mert" className="avatar"/>
           <p>
-            I am Mert Özkaynak, a Washington DC based creative developer.
+            I am Mert Özkaynak, a Washington DC based creative developer with experience building RESTful APIs and reusable user interfaces for web and mobile apps.
+           
             {/* Currently a freelancer, working on personal projects and prioritizing clients as they come. */}
             {/* Currently working as a freelancer, prioritizing client products as they come while working on personal projects. */}
             {/* Although my passion is to build products that bring people together, I embrace building products in different industries which allows me to gain more knowledge. */}
@@ -26,7 +27,16 @@ const About = () => {
             In coding I recognized and embraced the process from basketball, this time spending countless hours on Google rather than on the hardwood, and to tackle world problems rather than for sport, which fuels me everyday.  */}
           </p>
           <p>
-            Pleasure to meet you. <span>What is your story?</span>
+            Technology was not my first career path although I encountered computers at a very young age to play video games. 
+            In high school I took graphic design courses and learned how to photoshop myself into 2k covers as well as to draw logos on the Illustrator. 
+            In college I was making house and hip-hop beats on Fruity Loops Studio, while I took video editing courses and learned Adobe Premiere.
+            Although I enjoyed being a creative with these tools, I didn't see them more than hobbies.
+          </p>
+          <p>
+            Once my basketball career came to an end, I saw programming as the next step and attended the Flatiron Bootcamp to learn full-stack web development with Ruby on Rails and React.
+            While I enjoy building both sides of apps and handling their integrations, I found a love for frontend development for the creative joy it brings to design the user experience.
+          {/* frontend development as designing the user experience invokes creative joy. */}
+            {/* Pleasure to meet you. <span>What is your story?</span> */}
           </p>
         </div>
         </animated.div>
